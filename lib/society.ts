8fa6e3da1c9c543443f19248d5a176965ef41e8e
markoz <markdrew53@gmail.com>
2019-12-14 14:36:04 -0600

    Added society list

---
+++ lib/society.ts
@@ -0,0 +1,31 @@
+// tslint:disable-next-line: interface-name
+interface Society {
+  label: string;
+  value: string;
+}
+
+const societys: Society[] = [
+  { label: 'Argentina', value: 'Argentina' },
+  { label: 'Bolivia', value: 'Bolvia' },
+  { label: 'Brasil', value: 'Brasil' },
+  { label: 'Chile', value: 'Chile' },
+  { label: 'Colombia', value: 'Colombia' },
+  { label: 'Costa Rica', value: 'Costa rica' },
+  { label: 'Cuba', value: 'Cuba' },
+  { label: 'República Dominicana', value: 'República Dominicana' },
+  { label: 'Ecuador', value: 'Ecuador' },
+  { label: 'El Salvador', value: 'El Salvador' },
+  { label: 'Guatemala', value: 'Guatemala' },
+  { label: 'Haití', value: 'Haití' },
+  { label: 'Honduras', value: 'Honduras' },
+  { label: 'México', value: 'México' },
+  { label: 'Nicaragua', value: 'Nicaragua' },
+  { label: 'Panamá', value: 'Panamá' },
+  { label: 'Paraguay', value: 'Paraguay' },
+  { label: 'Perú', value: 'Perú' },
+  { label: 'Puerto Rico', value: 'Puerto Rico' },
+  { label: 'Uruguay', value: 'Uruguay' },
+  { label: 'Venezuela', value: 'Venezuela' }
+];
+
+export { societys };
