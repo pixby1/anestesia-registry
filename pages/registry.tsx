3728b9d38330e0bfe87fe2dccb0b7ffa83cc4167
markoz <markdrew53@gmail.com>
2020-02-29 17:58:31 -0600

    Added filed

--- pages/registry.tsx
+++ pages/registry.tsx
@@ -4,7 +4,9 @@ import * as Yup from 'yup';
 import { Button, FormLabel, Input } from '@chakra-ui/core';
 import { Radio, RadioGroup } from 'react-radio-group';
 import { Form, Formik, useField } from 'formik';
-import React, { useState } from 'react';
+import { useState } from 'react';
+import PhoneInput from 'react-phone-input-2';
+import DatePicker from 'react-datepicker';
 import Select from 'react-select';
 import tinytime from 'tinytime';
 
@@ -15,21 +17,41 @@ import { Layout } from '../components/Layout';
 import { societys } from '../lib/society';
 import { countries } from '../lib/country';
 
-interface CountrySelect {
-  label?: string;
-}
-
-interface SocietySelect {
-  label?: string;
-}
+const countriesPhone: string[] = [
+  'ar',
+  'bo',
+  'br',
+  'cl',
+  'co',
+  'cr',
+  'cu',
+  'do',
+  'ec',
+  'sv',
+  'gt',
+  'ht',
+  'hn',
+  'mx',
+  'ni',
+  'pa',
+  'py',
+  'pe',
+  'pr',
+  'uy',
+  've'
+];
 
 interface FormValues {
   name: string;
   lastName: string;
   email: string;
+  birthday: Date;
+  gender: string;
+  phone: string;
   jobRole: string;
-  country: CountrySelect | any;
-  society: SocietySelect | any;
+  country: string | any;
+  city: string;
+  society: string | any;
 }
 
 interface SelectProps {
@@ -42,8 +64,27 @@ interface SelectProps {
   error: any;
 }
 
-interface RadioProps {
+interface PhoneProps {
   value: string;
+  onChange: any;
+  onBlur: any;
+  error: any;
+}
+
+interface DatePickerProps {
+  value: any;
+  onChange: any;
+  onBlur: any;
+  error: any;
+}
+
+interface RadioProps {
+  name: string;
+  label: string;
+  label2: string;
+  value: string;
+  valueRadio: string;
+  valueRadio2: string;
   onChange: any;
   error: any;
 }
@@ -67,26 +108,26 @@ const MessageError = ({ title }: MessageProps) => {
 };
 
 const RadioButton = (props: RadioProps) => {
-  const { value, onChange, error } = props;
+  const { name, label, label2, value, valueRadio, valueRadio2, onChange, error } = props;
   return (
-    <>
+    <div style={{ marginBottom: '1em' }}>
       <RadioGroup
         selectedValue={value}
         onChange={select => {
-          onChange('jobRole', select);
+          onChange(name, select);
         }}
       >
         <label style={{ marginRight: 10 }}>
-          <Radio style={{ marginRight: 5 }} value="anestesiólogo" />
-          anestesiólogo
+          <Radio style={{ marginRight: 5 }} value={valueRadio} />
+          {label}
         </label>
         <label>
-          <Radio style={{ marginRight: 5 }} value="residente" />
-          residente
+          <Radio style={{ marginRight: 5 }} value={valueRadio2} />
+          {label2}
         </label>
       </RadioGroup>
       <MessageError title={error} />
-    </>
+    </div>
   );
 };
 
@@ -110,26 +151,6 @@ const MySelect = (props: SelectProps) => {
   );
 };
 
-const initialValues: FormValues = {
-  name: '',
-  lastName: '',
-  email: '',
-  jobRole: '',
-  country: '',
-  society: ''
-};
-
-const yupSchema = Yup.object({
-  name: Yup.string().required('Requerido'),
-  lastName: Yup.string().required('Requerido'),
-  email: Yup.string()
-    .email('Email invalidado')
-    .required('Requerido'),
-  jobRole: Yup.string().required('Requerido'),
-  country: Yup.string().required('Requerido'),
-  society: Yup.string().required('Requerido')
-});
-
 const TextInput = ({ label, ...props }: any) => {
   const [field, meta] = useField(props);
   return (
@@ -141,6 +162,82 @@ const TextInput = ({ label, ...props }: any) => {
     </>
   );
 };
+
+const InputPhone = (props: PhoneProps) => {
+  const { value, onChange, onBlur, error } = props;
+  return (
+    <div style={{ marginBottom: '1em' }}>
+      <PhoneInput
+        onlyCountries={countriesPhone}
+        searchPlaceholder="Tu país..."
+        placeholder="Escriba tu teléfono aqui..."
+        enableSearch={true}
+        value={value}
+        onChange={valuePhone => {
+          onChange('phone', valuePhone);
+        }}
+        onFocus={() => {
+          onBlur('phone', true);
+        }}
+      />
+      <MessageError title={error} />
+    </div>
+  );
+};
+
+const DateBirthday = (props: DatePickerProps) => {
+  const { value, onChange, onBlur, error } = props;
+  return (
+    <div style={{ marginBottom: '1em' }}>
+      <DatePicker
+        selected={value}
+        onChange={selectValue => {
+          onChange('birthday', selectValue);
+        }}
+        onFocus={() => {
+          onBlur('birthday', true);
+        }}
+        isClearable
+        peekNextMonth
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
+      />
+      <MessageError title={error} />
+    </div>
+  );
+};
+
+const initialValues: FormValues = {
+  name: '',
+  lastName: '',
+  email: '',
+  birthday: new Date(),
+  gender: '',
+  phone: '',
+  jobRole: '',
+  country: '',
+  city: '',
+  society: ''
+};
+
+const yupSchema = Yup.object({
+  name: Yup.string().required('Requerido'),
+  lastName: Yup.string().required('Requerido'),
+  email: Yup.string()
+    .email('Email invalidado')
+    .required('Requerido'),
+  birthday: Yup.date()
+    .typeError('Ingrese una fecha de nacimiento')
+    .required('Ingrese una fecha de nacimiento'),
+  gender: Yup.string().required('Requerido'),
+  phone: Yup.string().required('Requerido'),
+  jobRole: Yup.string().required('Requerido'),
+  country: Yup.string().required('Requerido'),
+  city: Yup.string().required('Requerido'),
+  society: Yup.string().required('Requerido')
+});
+
 const Registry = () => {
   const [succesRegistry, setRegistry] = useState<boolean>(false);
   return (
@@ -156,9 +253,13 @@ const Registry = () => {
             body: JSON.stringify({
               name: values.name,
               lastName: values.lastName,
+              birthday: template.render(values.birthday),
               email: values.email,
+              phone: values.phone,
+              gender: values.gender,
               jobRole: values.jobRole,
               country: values.country?.label,
+              city: values.city,
               society: values.society?.label,
               date: template.render(new Date())
             })
@@ -186,8 +287,38 @@ const Registry = () => {
             <Form style={{ marginTop: '2em' }}>
               <TextInput label="Nombre" name="name" type="text" />
               <TextInput label="Apellido" name="lastName" type="text" />
+              <FormLabel>Fecha de Nacimiento</FormLabel>
+              <DateBirthday
+                value={values.birthday}
+                onChange={setFieldValue}
+                onBlur={setFieldTouched}
+                error={errors.birthday}
+              />
               <TextInput label="Email" name="email" type="email" />
-              <RadioButton value={values.jobRole} onChange={setFieldValue} error={errors.jobRole} />
+              <FormLabel>Teléfono</FormLabel>
+              <InputPhone value={values.phone} onChange={setFieldValue} onBlur={setFieldTouched} error={errors.phone} />
+              <FormLabel>Genero</FormLabel>
+              <RadioButton
+                name="gender"
+                label="Masculino"
+                label2="Femenino"
+                value={values.gender}
+                valueRadio="masculino"
+                valueRadio2="femenino"
+                onChange={setFieldValue}
+                error={errors.gender}
+              />
+              <FormLabel>Categoría</FormLabel>
+              <RadioButton
+                name="jobRole"
+                label="Anestesiólogo"
+                label2="Residente"
+                value={values.jobRole}
+                valueRadio="anestesiólogo"
+                valueRadio2="residente"
+                onChange={setFieldValue}
+                error={errors.jobRole}
+              />
               <FormLabel>País</FormLabel>
               <MySelect
                 name="country"
@@ -198,6 +329,7 @@ const Registry = () => {
                 onBlur={setFieldTouched}
                 error={errors.country}
               />
+              <TextInput label="Ciudad" name="city" type="text" />
               <FormLabel>Sociedad</FormLabel>
               <MySelect
                 name="society"
@@ -211,7 +343,7 @@ const Registry = () => {
               <Button
                 type="submit"
                 mb="1em"
-                width="240px"
+                width="302px"
                 rounded="5px"
                 border="1px solid #111"
                 bg="#111"
