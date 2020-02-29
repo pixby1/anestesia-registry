6fbbb37707e756adb0b25fadc704626022b858e2
markoz <markdrew53@gmail.com>
2020-02-29 15:13:20 -0600

    Adding global styles for DatePicker

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import 'react-phone-number-input/style.css';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function MyApp({ Component, pageProps }: any) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
